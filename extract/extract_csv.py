59262ae23419524ae6661257b15ba801dc63d788
Hanna <hanna.mcadam@outlook.com>
2025-03-25 12:41:43 -0400

    put limit var back to 50k

--- extract/extract_csv.py
+++ extract/extract_csv.py
@@ -6,7 +6,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def extract_tree_csv_data():
     base_url = "https://data.cityofnewyork.us/resource/uvpi-gqnh.csv"
-    limit = 5000
+    limit = 50000
     offset = 0
     all_chunks = []
 
@@ -22,7 +22,7 @@ def extract_tree_csv_data():
         all_chunks.append(chunk)
         offset += limit
 
-    #df = pd.read_csv(f"{base_url}?$limit={limit}")
+    #df = pd.read_csv(f"{base_url}?$limit=5000")
     # combine all chunks into one df
     df = pd.concat(all_chunks, ignore_index=True)
     print(f"Total rows retrieved: {len(df)}")
